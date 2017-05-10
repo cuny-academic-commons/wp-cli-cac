5a9f8f364daab5722ed204637b7a5d82e1722f67
Boone B Gorges <boonebgorges@gmail.com>
2017-05-10 13:15:23 -0500

    Add upgrade notice about Basic theme.

--- wp-cli-cac.php
+++ wp-cli-cac.php
@@ -108,6 +108,7 @@ class CAC_Command extends WP_CLI_Command {
 		WP_CLI::log( 'Don\'t forget to manually check WooThemes for available updates.' );
 		WP_CLI::log( 'Also, don\'t forget to manually check https://wpcom-themes.svn.automattic.com for updates to "imbalance2" and "manifest".' );
 		WP_CLI::log( 'Also, don\'t forget to manually check http://themetrust.com for updates to "reveal".' );
+		WP_CLI::log( 'Also, don\'t forget to manually check https://themify.me/themes/basic for updates to "basic".' );
 
 		WP_CLI::success( 'All done! Be sure to review the release manifest (.cac-major-update.json) before checking into the repo.' );
 	}
